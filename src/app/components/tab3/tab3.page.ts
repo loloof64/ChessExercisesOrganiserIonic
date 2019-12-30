6091b1e9a1259f16c6787a067412658022ee8456
Laurent Bernabe <laurent.bernabe@gmail.com>
2019-12-30 19:16:45 +0100

    Added loader for when board is busy.

--- src/app/components/tab3/tab3.page.ts
+++ src/app/components/tab3/tab3.page.ts
@@ -1,4 +1,5 @@
-import { Component, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
+import { Component, ViewChild, OnInit, DoCheck, AfterViewInit } from '@angular/core';
+import { Platform } from '@ionic/angular';
 import { PlayerType } from '../loloof64-chessboard/PlayerType';
 import { Loloof64ChessboardComponent } from '../loloof64-chessboard/loloof64-chessboard.component';
 
@@ -7,14 +8,43 @@ import { Loloof64ChessboardComponent } from '../loloof64-chessboard/loloof64-che
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss']
 })
-export class Tab3Page implements AfterViewInit {
+export class Tab3Page implements  OnInit, DoCheck, AfterViewInit {
 
   @ViewChild('chessBoard', {static: true}) chessBoard: Loloof64ChessboardComponent;
 
+  boardSize: number;
+  boardBusy = true;
+
+  constructor(private platform: Platform) {}
+
+  private adjustBoardSize = () => {
+    this.boardSize = this.platform.isPortrait() ?
+      this.platform.width() :
+      this.platform.height() - 56;
+  }
+
+  ngOnInit() {
+    this.adjustBoardSize();
+  }
+
+  ngDoCheck() {
+    this.adjustBoardSize();
+  }
+
   ngAfterViewInit() {
     setTimeout(() => {
+      this.boardBusy = false;
       this.chessBoard.startNewGame(PlayerType.Computer, PlayerType.Human, undefined);
-    }, 1200);
+    }, 2500);
   }
+
+  hideLoader() {
+    this.boardBusy = false;
+  }
+
+  showLoader() {
+    this.boardBusy = true;
+  }
+
 
 }
